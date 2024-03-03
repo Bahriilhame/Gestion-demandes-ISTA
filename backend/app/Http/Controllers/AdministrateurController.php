9a972dfc3466c7553f2a1e7926c153d92a73c42a
bahriilhame <bahri.ilhame@gmail.com>
2024-03-03 18:44:41 +0100

    added the store methode for the Administrateur

--- backend/app/Http/Controllers/AdministrateurController.php
+++ backend/app/Http/Controllers/AdministrateurController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Administrateur;
 
 class AdministrateurController extends Controller
 {
-    //
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom' => 'required|string|max:255',
+            'prenom' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:administrateurs|max:255',
+            'mdp' => 'required|string|min:8',
+            'role' => 'required|string|in:directeur,gestionnaire',
+        ]);
+
+        $gestionnaire = Administrateur::create([
+            'nom' => $request->nom,
+            'prenom' => $request->prenom,
+            'email' => $request->email,
+            'mdp' => bcrypt($request->mdp),
+            'role' => $request->role,
+        ]);
+
+        return response()->json($gestionnaire, 201);
+    }
+
 }
