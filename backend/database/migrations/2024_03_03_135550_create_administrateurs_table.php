84bc571bc10fad3500d064c27d06132d819deace
bahriilhame <bahri.ilhame@gmail.com>
2024-03-03 14:58:19 +0100

    created the administrateurs migration

--- backend/database/migrations/2024_03_03_135550_create_administrateurs_table.php
+++ backend/database/migrations/2024_03_03_135550_create_administrateurs_table.php
@@ -6,22 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+    public function up(){
         Schema::create('administrateurs', function (Blueprint $table) {
             $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('email')->unique();
+            $table->string('mdp');
+            $table->enum('role', ['directeur', 'gestionnaire']);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void{
         Schema::dropIfExists('administrateurs');
     }
 };
