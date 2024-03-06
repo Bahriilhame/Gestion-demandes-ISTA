e03bf36adaf7a994059031464bc8b8cbc40456ca
bahriilhame <bahri.ilhame@gmail.com>
2024-03-06 11:58:54 +0100

    added the succès component

---
+++ frontend/src/Toast.jsx
@@ -0,0 +1,26 @@
+
+import { useEffect } from 'react';
+import { ToastContainer, toast,Slide  } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Toast(){
+    useEffect(()=>{
+        toast.success('Ajouter avec succès', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Slide,
+        });
+    })
+
+  return (
+    <div>
+      <ToastContainer />
+    </div>
+  );
+}
