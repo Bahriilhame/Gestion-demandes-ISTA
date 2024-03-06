80bbe7980d2579aa4f4a8156f4d7d27be31c43cf
bahriilhame <bahri.ilhame@gmail.com>
2024-03-06 12:08:28 +0100

    added the route in the main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import FormStagiaire from './Components/FormStagiaire';
-import ListStagiaires from './Components/ListStagiaires';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DashboardDirect from './DashboardDirect';
+import DashboardGest from './DashboardGest';
 import './index.css';
+import ListStagiaires from './Components/ListStagiaires';
+import FormStagiaire from './Components/FormStagiaire';
 import AddGestionnaire from './Components/AddGestionnaire';
 import ListeGestionnaires from './Components/ListeGestionnaires';
 
@@ -11,32 +13,18 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <div className="flex h-screen">
-        <nav className="bg-[#00246B] text-white p-4">
-          <ul className="flex flex-col">
-            <div className="flex items-center align-middle mr-4 mb-8">
-              <img src="/logo.png" alt="logo" className="w-12 h-12 mr-2" />
-              <span className='align-middle font-bold'>ISTA HAY SALAM</span>
-            </div>
-            <li className="mb-4">
-              <Link to='/' className="hover:text-gray-300">Liste des stagiaires</Link>
-            </li>
-            <li className="mb-4">
-              <Link to='/AddStagiaire' className="hover:text-gray-300">Form Ajout des stagiaires</Link>
-            </li>
-            <li className="mb-4">
-              <Link to='/listGestionnaire' className="hover:text-gray-300">Liste des Gestionnaires</Link>
-            </li>
-            <li className="mb-4">
-              <Link to='/AddGestionnaire' className="hover:text-gray-300">Form Ajout des Gestionnaires</Link>
-            </li>
-          </ul>
-        </nav>
         <div className="flex-grow">
           <Routes>
-            <Route path='/' element={<ListStagiaires />}/>
-            <Route path='/AddStagiaire' element={<FormStagiaire />}/>
-            <Route path='/AddGestionnaire' element={<AddGestionnaire />}/>
-            <Route path='/listGestionnaire' element={<ListeGestionnaires />}/>
+            <Route path='/dashboard-directeur' element={<DashboardDirect/>}>
+              <Route path="listDemandes" element={<ListStagiaires />} />
+              <Route path="AddStagiaire" element={<FormStagiaire />} />
+              <Route path="listGestionnaire" element={<ListeGestionnaires />} />
+              <Route path="AddGestionnaire" element={<AddGestionnaire />} />
+            </Route>
+            <Route path='/dashboard-gestionnaire' element={<DashboardGest />}>
+              <Route path="listDemandes" element={<ListStagiaires />} />
+              <Route path="AddStagiaire" element={<FormStagiaire />} />
+            </Route>
           </Routes>
         </div>
       </div>
